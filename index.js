4e7fd8be56dc97e8a9cde44883f3e3fc40c497fd
Abdirashidnoor <abdirashid16112@gmail.com>
2023-03-19 23:25:28 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution in this file!
+// Write your solution in this file
+// Define the initial employee object
+let employee = {
+    name: 'Sam'
+};
+
+{ Object } employee
+{ string } key
+{ string } value
+{ Object }
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, { [key]: value });
+}
+
+
+{ Object } employee
+{ string } key
+{ string } value
+{ Object }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+
+{ Object } employee
+{ string } key
+{ Object }
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+
+{ Object } employee
+{ string } key
+{ Object }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
